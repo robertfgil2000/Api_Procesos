9f7bdc35aa4a7855d24c7a51c35ece823ef40c27
EsperanzaGO <95490448+EsperanzaGO@users.noreply.github.com>
2021-12-13 09:59:39 -0500

    Update count.c

--- Punto A/count.c
+++ Punto A/count.c
@@ -3,16 +3,20 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+	
     int contador = 1;
-  
+	
+  //Variable que contendrá la función fork()
     int rc = fork();
 	
     if (rc < 0) {
-        fprintf(stderr, "fork failed\n");
+        fprintf(stderr, "Error. Fork ha fallado\n");
         exit(1);
     }else if (rc == 0) {
+	//Proceso hijo
         contador+= 5;
     }else {
+	//Proceso padre
         contador+= 2;
     }
     printf("%d\n", contador);
